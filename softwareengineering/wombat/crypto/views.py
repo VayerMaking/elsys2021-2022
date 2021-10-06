349f264068a7046ea0a57982055c0b72bc28ec7e
VayerMaking <36741716+VayerMaking@users.noreply.github.com>
2021-10-06 17:29:42 +0300

    quieries and views

--- softwareengineering/wombat/crypto/views.py
+++ softwareengineering/wombat/crypto/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Currency
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def trending(request):
+    trending = Currency.objects.order_by('-price')[0]
+    return HttpResponse("The currently trending currency is: " + str(trending))
