039a011f5595421c90f31c071c2b0dbe52db663e
VayerMaking <36741716+VayerMaking@users.noreply.github.com>
2021-10-13 16:51:25 +0300

    update gitignore and update models

--- softwareengineering/wombat/crypto/models.py
+++ softwareengineering/wombat/crypto/models.py
@@ -5,6 +5,8 @@ class Currency(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()  # price in USD
     code = models.CharField(max_length=3)  # eg btc , eth
+    supply = models.PositiveIntegerField(default=100000)
+    description = models.CharField(max_length=1000, default="some description")
 
     def __str__(self) -> str:
         return self.code + " " + str(self.price)
